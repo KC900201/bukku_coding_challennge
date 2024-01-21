a750e9df849ce88bce5d9fe44269709151067ade
KC Ng <nkc900201@gmail.com>
2024-01-21 13:54:55 +0900

    Fix title spelling

--- src/components/molecules/LandingPage.tsx
+++ src/components/molecules/LandingPage.tsx
@@ -11,7 +11,7 @@ function LandingPage({
     <section className="bg-white">
       <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
         <h2 className="text-2xl font-bold tracking-tight text-gray-900">
-          {isMerchant ? 'Available of Items' : 'Items on Sale'}
+          {isMerchant ? 'Available Items' : 'Items on Sale'}
         </h2>
         {/* Product listing */}
         <div className="mt-6 grid grid-cols-1 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
